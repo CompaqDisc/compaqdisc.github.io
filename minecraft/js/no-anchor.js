0b5a29ba3e25e3e392ff89ff12d6382af5919f02
Herobrine2Nether <18bradan.wolbeck@gmail.com>
2015-05-07 15:45:29 -0600

    Commit

--- minecraft/js/no-anchor.js
+++ minecraft/js/no-anchor.js
@@ -1,8 +1,12 @@
-$(window).load(function(){
-(function($) {
-    $(document).ready(function() {
-    var url=document.URL.split("#");
-    var anchor=url[1];
-    });
-})(jQuery);
-});
+
+	$(function() {
+	  $('a[href*=#]:not([href=#])').click(function() {
+	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+
+	      var target = $(this.hash);
+	      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	      
+	    }
+	  });
+	});
+	
